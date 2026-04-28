1273eaf255a711d33e8ba5581a242ed8a5ea9b85
cadusantosofc <cadusantos.paiva@gmail.com>
2026-04-28 19:41:31 -0300

    Corrigir env e seed

--- seed_questions.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Arquivo desativado. 
-// O conteúdo agora é gerenciado 100% via Migrações SQL (040_seed_logica_v5.sql, etc)
-// para garantir integridade referencial e evitar TRUNCATE acidental.
-if (isset($isMigrationCall)) {
-    echo "<h4>>> Sistema de conteúdo migrado para SQL (V5).</h4>";
-}
